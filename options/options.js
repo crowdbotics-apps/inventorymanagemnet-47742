3a63984d205df34ac30b4743841cec842af1705b
Crowdbotics <team@crowdbotics.com>
2024-04-29 21:07:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "inventorymanagemnet-47742",
+  url: "https://inventorymanagemnet-47742.botics.co",
+  api: "https://inventorymanagemnet-47742.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
